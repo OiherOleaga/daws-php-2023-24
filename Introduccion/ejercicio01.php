52b0ad01261a560043003767133f96b78e2fa237
OiherOleaga <oiher.oleaga@ikasle.egibide.org>
2023-10-20 12:57:54 +0200

    ejercicio1 completado

---
+++ Introduccion/ejercicio01.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>PHP prueba</title>
+
+    <head>
+
+    <body>
+        <h1>. Crea dos variables llamadas nombre y edad. La variable nombre tendrá el
+            valor “Mikel” y la variable edad tendrá asignado el valor 22. A continuación
+            muéstralas por pantalla.</h1>
+        <?php
+        $nombre = 'Mikel';
+        $edad = 22;
+
+        echo ('<p>' . $nombre . $edad . '</p>')
+        ?>
+    </body>
+
+</html>
