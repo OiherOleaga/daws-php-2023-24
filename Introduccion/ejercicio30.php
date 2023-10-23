c3b331b89b3c9bd39955025687f3fbc2481abf5d
OiherOleaga <oiher.oleaga@ikasle.egibide.org>
2023-10-23 13:39:44 +0200

    ejercicio 30

---
+++ Introduccion/ejercicio30.php
@@ -0,0 +1,10 @@
+<?php
+$estudiantes = array(
+    "Juan" => 8.5,
+    "Guti" => 7.2,
+    "Iker" => 2.0,
+    "Pedro" => 6.8,
+    "Antonio" => 8.0,
+    "Arturo" => 7.5
+);
+include "ejercicio30.view.php";
---
+++ Introduccion/ejercicio30.view.php
@@ -0,0 +1,31 @@
+    <!DOCTYPE html>
+    <html>
+
+    <head>
+        <title>PHP prueba</title>
+
+        <head>
+
+        <body>
+            <p>
+                Crea un array asociativo con el nombre de 6 estudiantes y la nota media de
+                cada uno. Recorre el array utilizando la estructura foreach y mostrando una lista
+                con frases con la siguiente estructura: “La nota media de Julen es 6.8”
+            </p>
+
+            <ls>
+                <?php
+                foreach ($estudiantes as $persona => $nota) {
+                ?>
+                    <li> La nota media de <?= $persona ?> es <?= $nota ?></li>
+
+                <?php
+                }
+                ?>
+
+            </ls>
+
+
+        </body>
+
+    </html>
