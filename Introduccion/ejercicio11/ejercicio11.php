d378b34fe284fd001bf7c71eb22970442117bf69
OiherOleaga <oiher.oleaga@ikasle.egibide.org>
2023-10-23 09:37:03 +0200

    ejercicio11

---
+++ Introduccion/ejercicio11/ejercicio11.php
@@ -0,0 +1,13 @@
+<?php
+$a = $_GET["cad1"];
+$b = $_GET["cad2"];
+$respuesta = concat($a, $b);
+
+function concat($a, $b = "me llamo alberto")
+{
+    $cadenasUnidas = $a . $b;
+
+    return $cadenasUnidas;
+};
+require "ejercicio11.view.php";
+echo ("<p>las dos cadenas unidas son: $respuesta </p>");
---
+++ Introduccion/ejercicio11/ejercicio11.view.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>PHP prueba</title>
+
+    <head>
+
+    <body>
+        <p>
+            Separa el código del ejercicio anterior en dos archivos distintos. Uno de ellos
+            tendrá todo el contenido relacionado con la presentación de la página y el otro
+            deberá contener todo aquello relacionado con los datos y la lógica de la
+            aplicación
+
+        </p>
+    </body>
+
+</html>
